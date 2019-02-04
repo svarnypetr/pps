dd9c8e163b779d9869de2684e0a8c267a5421ef3
Petr Svarny <svarnpet@fel.cvut.cz>
2019-02-04 22:39:05 +0100

    camera_tf at experiment position

--- scripts/camera_tf.py
+++ scripts/camera_tf.py
@@ -10,7 +10,7 @@ from visualization_msgs.msg import Marker
 def create_camera_marker(trans, size=(0.005, 0.005, 0.005), color=(1.0, 1.0, 1.0)):
     camera_marker = Marker()
     camera_marker.id = 0
-    camera_marker.header.frame_id = '/world'
+    camera_marker.header.frame_id = 'r1_link_0'
     camera_marker.header.stamp = rospy.Time(0)
     camera_marker.type = camera_marker.MESH_RESOURCE
     camera_marker.mesh_resource = "package://pps_robot/meshes/camera/d435.stl"
@@ -57,19 +57,29 @@ def update_camera_marker(tf_listener):
 
 
 if __name__ == '__main__':
+
     rospy.init_node('camera_tf_broadcaster')
-    # br = tf.TransformBroadcaster()
+
+    BROADCAST_TF = True
+    # CAM_COORD = (-1.0, -1.0, 1.0)
+    # CAM_QUAT = quaternion_from_euler(-1.5707, 0, -1.5707)
+    CAM_COORD = (1.678, 0.912, 0.429)
+    CAM_QUAT = (-0.325, -0.62, 0.63, 0.334)
+
+    if BROADCAST_TF:
+        br = tf.TransformBroadcaster()
     listener = tf.TransformListener()
     rate = rospy.Rate(10.0)
 
     publisher = rospy.Publisher('camera_link', Marker, queue_size=100)
 
     while not rospy.is_shutdown():
-        # br.sendTransform((-1.0, -1.0, 1.0),
-        #                  quaternion_from_euler(-1.5707, 0, -1.5707),
-        #                  rospy.Time.now(),
-        #                  "camera",
-        #                  "r2_link_0")
+        if BROADCAST_TF:
+            br.sendTransform(,
+                          ,
+                          rospy.Time.now(),
+                          "camera",
+                          "r1_link_0")
         update_camera_marker(listener)
 
         rate.sleep()
