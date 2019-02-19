ed6ebe5a28475cf19b54adb8d7bf4c18270a3020
Michael Tesař <michael.tesar@cvut.cz>
2019-02-19 11:17:59 +0100

    Add basic robot logic

--- scripts/robot.py
+++ scripts/robot.py
@@ -1,104 +1,66 @@
 #!/usr/bin/python
-
 import rospy
-import numpy
+from math import radians, pi
 from capek_pycommander.capek_robot import CapekRobotCommander
-from moveit_msgs.msg import RobotState
-from geometry_msgs.msg import Pose
 from std_msgs.msg import Int8
-from copy import deepcopy
 
-STATE = 0
+template = [
+	[-radians(30), 0.0, 0.0, -radians(90), 0.0, radians(90), 0.0],
+	[-radians(30), radians(55), 0.0, -radians(90+20), 0.0, radians(15), 0.0],
+	[-radians(30), 0.0, 0.0, -radians(90), 0.0, radians(90), 0.0],
+	[+radians(30), 0.0, 0.0, -radians(90), 0.0, radians(90), 0.0],
+	[+radians(30), radians(55), 0.0, -radians(90+20), 0.0, radians(15), 0.0],
+	[+radians(30), 0.0, 0.0, -radians(90), 0.0, radians(90), 0.0],
+]
 
-# Initialize node and robot
-rospy.init_node("joint_space_move_example")
-crc = CapekRobotCommander("r1")
-crc.group.set_planner_id("RRTConnectkConfigDefault")
-crc.group.set_pose_reference_frame("r1_link_0")
-crc.group.set_max_velocity_scaling_factor(1.0)
-
-# Go to the names position L
-crc.move_l_position()
-
-# Set start state
-crc.group.set_start_state(RobotState())
-crc.group.clear_pose_targets()
-crc.group.allow_replanning(True)
-
-rospy.sleep(0.5)
-
-# Move to blue mark
-blue_mark = Pose()
-blue_mark.position.x = 0.45
-blue_mark.position.y = -0.3
-blue_mark.position.z = 0.3
-blue_mark.orientation.y = 1.0
-
-blue_down = deepcopy(blue_mark)
-blue_down.position.z = 0.1
-
-green_mark = Pose()
-green_mark.position.x = 0.65
-green_mark.position.y = 0.3
-green_mark.position.z = 0.3
-green_mark.orientation.y = 1.0
-
-green_down = deepcopy(green_mark)
-green_down.position.z = 0.1
-
-l_position = Pose()
-l_position.position.x = 0.5
-l_position.position.z = 0.5
-l_position.orientation.y = 1.0
-
-trajectories = list()
-template = [blue_mark, blue_down, blue_mark, green_mark, green_down, green_mark]
-for repetition in range(5):
-    if not repetition % 5:
-        trajectories.extend([l_position])
-    else:
-        trajectories.extend(template)
-trajectories.extend([l_position])
-
-cartesian_plan, fraction = crc.group.compute_cartesian_path(trajectories, 0.01, 0.0, True)
-#crc.display_trajectory(cartesian_plan)
-#crc.execute_plan(cartesian_plan)
-retimed_plan = crc.retime_trajectory(cartesian_plan, 0.1)
-
+N = 2
+REPETITION = 0
+FULL_SPEED = 1.0
+SLOW_SPEED = 0.2
+INDEX = 0
+STATUS = 0
 
 def pps_callback(msg):
-    status = msg.data
-    if status == 2:
-        rospy.loginfo("Stop: Possible collision with human")
-        crc.stop()
-        crc.group.set_start_state(RobotState())
-        crc.group.clear_pose_targets()
-    if status == 3:
-        rospy.loginfo("Start: Executing plan from beginning")
-        crc.group.set_start_state(RobotState())
-        crc.group.clear_pose_targets()
-        crc.move_l_position()
-        crc.group.set_start_state(RobotState())
-        crc.group.clear_pose_targets()
-        crc.execute_plan_async(cartesian_plan)
-    if status == 4:
-        rospy.loginfo("Debugging: Executing plan from beginning at 20% of speed")
-        crc.group.set_start_state(RobotState())
-        crc.group.clear_pose_targets()
-        crc.move_l_position()
-        crc.group.set_start_state(RobotState())
-        crc.group.clear_pose_targets()
-        crc.execute_plan_async(retimed_plan)
-    if status == 5:
-        rospy.loginfo("Reset: Moving to the L position")
-        crc.group.set_start_state(RobotState())
-        crc.group.clear_pose_targets()
-        crc.group.allow_replanning(True)
-        crc.move_l_position()
-        crc.group.set_start_state(RobotState())
-        crc.group.clear_pose_targets()
+	global STATUS
+	global INDEX
+	global REPETITION
+	global FULL_SPEED
+	global SLOW_SPEED
+	global N
+	
+	STATUS = msg.data
+	rate = rospy.Rate(10)
+	rate.sleep()
 
+if __name__ == "__main__":									
+	joints = list()
+	for _ in range(N):
+		joints.extend(template)
 
-pps_subscriber = rospy.Subscriber("pps_status", Int8, pps_callback)
+	rospy.init_node("move")
+	crc = CapekRobotCommander("r1")
+	crc.move_l_position()
 
-rospy.spin()
+	pps_subscriber = rospy.Subscriber("pps_status", Int8, pps_callback)
+	
+	rate = rospy.Rate(10)
+	
+	while not rospy.is_shutdown():
+		for index, coordinate in enumerate(joints[INDEX:]):
+			if not STATUS == 2:
+				if STATUS == 1:
+					crc.set_speed(SLOW_SPEED)
+				if STATUS == 3:
+					crc.set_speed(FULL_SPEED)
+				crc.group.set_start_state_to_current_state()
+				joint_plan = crc.plan_joint_goal(coordinate)
+				crc.display_trajectory(joint_plan)
+				crc.group.execute(joint_plan, wait=False)			
+				while True:
+					c1 = sum(crc.group.get_current_joint_values())
+					rate.sleep()
+					c2 = sum(crc.group.get_current_joint_values())
+					if round(c1, 6) == round(c2, 6):
+						INDEX = index % 6
+						break
+
